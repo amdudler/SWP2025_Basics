06b75232e84b5d843aa6ee318fc178a01891622f
amdudler <amir.duratovic@student.htldornbirn.at>
2024-09-13 13:06:00 +0200

    IF_Bedingungen_Aufgabe1 in progress

---
+++ src/IF_Bedingung_Aufgabe1.java
@@ -0,0 +1,22 @@
+
+
+package at.ran.basics;
+import java.util.Random;
+
+public class IF_Bedingung_Aufgabe1 {
+        public static void main(String[] args) {
+// Erstelle eine Zufallszahl zwischen 0 und 100
+            Random random = new Random();
+            int randomNumber = random.nextInt(100);
+// Gib die Zufallszahl aus
+            if(randomNumber>50){
+                console.log(randomNumber)
+            }
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
+        }
+
+    }
